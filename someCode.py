e7d8849b9847ec582cce9856723abe72a37bb75b
SpectatorRus <151949448+SpectatorRus@users.noreply.github.com>
2024-08-03 11:52:45 +0300

    Update someCode.py
    unused library removed

--- someCode.py
+++ someCode.py
@@ -1,5 +1,4 @@
 import logging
-from uuid import uuid4
 
 from telegram import InlineQueryResultArticle, InputTextMessageContent, Update
 from telegram.ext import Application, CommandHandler, ContextTypes, InlineQueryHandler, filters, MessageHandler
@@ -17,6 +16,7 @@ logging.basicConfig(
 logging.getLogger("httpx").setLevel(logging.WARNING)
 
 logger = logging.getLogger(__name__)
+
 
 def get_answer_from_gigachad(context):
     connection_to_gigachat = GigaChat(credentials=GIGACHAT_API_KEY, scope="GIGACHAT_API_PERS", verify_ssl_certs=False)
@@ -39,18 +39,18 @@ async def help_command(update: Update, context: ContextTypes.DEFAULT_TYPE) -> No
     if message_len > 2:
         return await update.message.reply_text("/help may have only 0,1 or 2 parameters, send /help help for full documentation")
     form_p_help = {"description": "<b>form_product</b>: составляет основные шаги к реализации проекта,краткий функционал и описание инструментов которые могут помочь в реализации, <b>не генерирует название продукта</b>\n",
-                         "syntax": "<b>синтаксис</b>: /form_product что делаем? язык программирования\n",
-                         "example": "<b>пример</b>: /form_product мессенджер python\n"}
+                   "syntax": "<b>синтаксис</b>: /form_product что делаем? язык программирования\n",
+                   "example": "<b>пример</b>: /form_product мессенджер python\n"}
     h_help = {"description": "<b>help</b> - вспомогательная команда, может вывести описание и синтаксис всех команд или вывести отдельный параметр у какой-либо команды\n",
-            "syntax": "<b>синтаксис</b>: /help название команды(не обязательно) параметр(не обязательно)\n <b>Параметры</b>: \n description - описание комадны\n syntax - синтаксис команды\n example - пример как можно написать комманду\n",
-            "example": "<b>пример без аргументов</b>: /help \n <b>1 аргумент</b>: /help команда \n <b>2 аргумента</b>: /help команда параметр\n"}
+              "syntax": "<b>синтаксис</b>: /help название команды(не обязательно) параметр(не обязательно)\n <b>Параметры</b>: \n description - описание комадны\n syntax - синтаксис команды\n example - пример как можно написать комманду\n",
+              "example": "<b>пример без аргументов</b>: /help \n <b>1 аргумент</b>: /help команда \n <b>2 аргумента</b>: /help команда параметр\n"}
     info_ab_help = {"description": "<b>info_about</b>: рассказывает какие библиотеки указанного языка программирования можно использовать для указанной задачи\n",
                     "syntax": "<b>синтаксис</b>: /info_about язык программирования задача. P.S если задачу нельзя описать одним словом - вместо пробелов используйте _ , например_так\n",
                     "example": "<b>пример</b>: /info_about python графики\n"}
     info_ab_lib_help = {"description": "<b>info_about_library</b>: рассказывает как использовать указанную библиотеку на указанном языке программирования с примерами кода\n",
                         "syntax": "<b>синтаксис</b>: /info_about_library название библиотеки язык программирования\n",
                         "example": "<b>пример</b>: /info_about_library Matplotlib python\n"}
-    help_data = {"help": h_help,"form_product": form_p_help, "info_about": info_ab_help, "info_ab_lib_help": info_ab_lib_help}
+    help_data = {"help": h_help, "form_product": form_p_help, "info_about": info_ab_help, "info_ab_lib_help": info_ab_lib_help}
     keys = ["description", "syntax", "example"]
     answer = ""
 
@@ -77,14 +77,14 @@ async def help_command(update: Update, context: ContextTypes.DEFAULT_TYPE) -> No
     await update.message.reply_text(answer, parse_mode="HTML")
 
 
-async def unknown_command(update: Update, context:ContextTypes.DEFAULT_TYPE) -> None:
+async def unknown_command(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
     await update.message.reply_text("Sorry I don't understand this command, use /help for a list of commands.")
 
 
 def form_context(params):
     message = " ".join(["Ты программист, я хочу  сделать", params[0],
-    "на языке программирования", params[1], "сформируй шаги которые помогут мне в создании этого проекта",
-    "и также сформируй краткое описание функционала и набор технологий которые могут пригодится."])
+                        "на языке программирования", params[1], "сформируй шаги которые помогут мне в создании этого проекта",
+                        "и также сформируй краткое описание функционала и набор технологий которые могут пригодится."])
     return Chat(messages=[Messages(role=MessagesRole.USER, content=message)])
 
 
@@ -110,9 +110,11 @@ async def info_about(update: Update, context: ContextTypes.DEFAULT_TYPE):
     res = get_answer_from_gigachad(info_context(message_text))
     await update.message.reply_text(res.choices[0].message.content, parse_mode="Markdown")
 
+
 def info_about_library_context(params):
     message = "".join(["Ты программист,  расскажи мне как пользоваться ", params[0], "с примером кода на языке", params[1]])
     return Chat(messages=[Messages(role=MessagesRole.USER, content=message)])
+
 
 async def info_about_library(update: Update, context: ContextTypes.DEFAULT_TYPE):
     message_text = update.message.text.split()
@@ -121,6 +123,7 @@ async def info_about_library(update: Update, context: ContextTypes.DEFAULT_TYPE)
         return await update.message.reply_text("params error, use /help info_about_library for documentation")
     res = get_answer_from_gigachad(info_about_library_context(message_text))
     await update.message.reply_text(res.choices[0].message.content, parse_mode="Markdown")
+
 
 def main() -> None:
     """Run the bot."""
